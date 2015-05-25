400caa72757f55bf721cea4e3e4041fe1e427f41
Bressain Dinkelman <dinkelburt@gmail.com>
2015-05-25 15:12:25 -0600

    Start using eslint
    Still figuring things out, shaving all the yaks

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
   render() {
     return (
       <div>ohai!</div>
-    );
+    )
   }
-};
+}
 
 React.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
+var path = require('path')
 var webpack = require('webpack')
+
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000',
-    'webpack/hot/only-dev-server',
-    './js/app.js'
+    path.resolve(__dirname, 'app/main.js')
   ],
   output: {
     path: './dist',
@@ -13,7 +13,7 @@ module.exports = {
     loaders: [
       { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
       { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css?$/, loader: 'style!css' },
+      { test: /\.css?$/, loader: 'style!css' }
     ]
   },
   plugins: [
